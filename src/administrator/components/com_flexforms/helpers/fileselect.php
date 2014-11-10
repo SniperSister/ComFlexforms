f536f8510c9b6dc9b4ac0ea824774a55f1dd915b
David Jardin <d.jardin@djumla.de>
2014-11-10 17:44:26 +0100

    Layout selecter cleanup

--- src/administrator/components/com_flexforms/helpers/fileselect.php
+++ src/administrator/components/com_flexforms/helpers/fileselect.php
@@ -10,6 +10,11 @@
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
+/**
+ * Class FlexformsHelperFileselect
+ *
+ * @since  1.0.0
+ */
 class FlexformsHelperFileselect
 {
     /**
@@ -19,108 +24,41 @@ class FlexformsHelperFileselect
      */
     public static function getLayoutOptions()
     {
-        $client = JApplicationHelper::getClientInfo(0);
+        // Get the database object and a new query object.
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
 
-        // Get the extension.
-        $extension = "com_flexforms";
-        $extension = preg_replace('#\W#', '', $extension);
+        // Build the query.
+        $query->select('e.element, e.name')
+            ->from('#__extensions as e')
+            ->where('e.client_id = 0')
+            ->where('e.type = ' . $db->quote('template'))
+            ->where('e.enabled = 1');
 
-        // Get the view.
-        $view = "form";
+        // Set the query and load the templates.
+        $db->setQuery($query);
+        $templates = $db->loadObjectList('element');
 
-        // If a template, extension and view are present build the options.
-        if ($extension && $view && $client)
+        $options["com.default"] = JText::_('COM_FLEXFORMS_FORMS_FIELD_LAYOUT_OPTION_COMPONENT');
+
+        // Loop on all templates
+        if ($templates)
         {
-            // Get the database object and a new query object.
-            $db = JFactory::getDbo();
-            $query = $db->getQuery(true);
-
-            // Build the query.
-            $query->select('e.element, e.name')
-                ->from('#__extensions as e')
-                ->where('e.client_id = 0')
-                ->where('e.type = ' . $db->quote('template'))
-                ->where('e.enabled = 1');
-
-            // Set the query and load the templates.
-            $db->setQuery($query);
-            $templates = $db->loadObjectList('element');
-
-            // Build the search paths for component layouts.
-            $component_path = JPath::clean($client->path . '/components/' . $extension . '/views/' . $view . '/tmpl');
-
-            // Prepare array of component layouts
-            $component_layouts = array();
-
-            // Prepare the grouped list
-            $options = array();
-
-            // Add the layout options from the component path.
-            if (is_dir($component_path) && ($component_layouts = JFolder::files($component_path, '^[^_]*\.xml$', false, true)))
+            foreach ($templates as $template)
             {
-                foreach ($component_layouts as $i => $file)
+                $template_path = JPath::clean(JPATH_SITE . '/templates/' . $template->element . '/html/com_flexforms/form');
+
+                // Add the layout options from the template path.
+                if (is_dir($template_path) && ($files = JFolder::files($template_path, '^[^_]*\.php$', false, true)))
                 {
-                    // Attempt to load the XML file.
-                    if (!$xml = simplexml_load_file($file))
+                    if (count($files))
                     {
-                        unset($component_layouts[$i]);
-
-                        continue;
-                    }
-
-                    // Get the help data from the XML file if present.
-                    if (!$menu = $xml->xpath('layout[1]'))
-                    {
-                        unset($component_layouts[$i]);
-
-                        continue;
-                    }
-
-                    // Add an option to the component group
-                    $value = basename($file, '.xml');
-                    $text = JText::_('COM_FLEXFORMS_FORMS_FIELD_LAYOUT_OPTION_COMPONENT') . " - " . $value;
-                    $options["com." . $value] = $text;
-                }
-            }
-
-            // Loop on all templates
-            if ($templates)
-            {
-                foreach ($templates as $template)
-                {
-                    $template_path = JPath::clean($client->path . '/templates/' . $template->element . '/html/' . $extension . '/' . $view);
-
-                    // Add the layout options from the template path.
-                    if (is_dir($template_path) && ($files = JFolder::files($template_path, '^[^_]*\.php$', false, true)))
-                    {
-                        // Files with corresponding XML files are alternate menu items, not alternate layout files
-                        // so we need to exclude these files from the list.
-                        $xml_files = JFolder::files($template_path, '^[^_]*\.xml$', false, true);
-
-                        for ($j = 0, $count = count($xml_files); $j < $count; $j++)
+                        foreach ($files as $file)
                         {
-                            $xml_files[$j] = basename($xml_files[$j], '.xml');
-                        }
-
-                        foreach ($files as $i => $file)
-                        {
-                            // Remove layout files that exist in the component folder or that have XML files
-                            if ((in_array(basename($file, '.php'), $component_layouts))
-                             || (in_array(basename($file, '.php'), $xml_files)))
-                            {
-                                unset($files[$i]);
-                            }
-                        }
-
-                        if (count($files))
-                        {
-                            foreach ($files as $file)
-                            {
-                                // Add an option to the template group
-                                $value = "tpl." . $template->element . "." . basename($file, '.php');
-                                $text = JText::sprintf('COM_FLEXFORMS_FORMS_FIELD_LAYOUT_OPTION_TEMPLATE', $template->name) . " - " . basename($file, '.php');
-                                $options[$value] = $text;
-                            }
+                            // Add an option to the template group
+                            $value = "tpl." . $template->element . "." . basename($file, '.php');
+                            $text = JText::sprintf('COM_FLEXFORMS_FORMS_FIELD_LAYOUT_OPTION_TEMPLATE', $template->name) . " - " . basename($file, '.php');
+                            $options[$value] = $text;
                         }
                     }
                 }
