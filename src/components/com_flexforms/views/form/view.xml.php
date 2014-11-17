a7bb039189dcb68cde4a6960d7179a3fb622cde6
David Jardin <d.jardin@djumla.de>
2014-11-17 12:50:12 +0100

    Improved security by disabling unnecessary tasks and surpressing alternate outputs

---
+++ src/components/com_flexforms/views/form/view.xml.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version    %%COMPONENTVERSION%%
+ * @package    Flexforms
+ * @copyright  2014 David Jardin
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link       http://www.djumla.de
+ */
+
+/**
+ * Class FlexformsViewForm
+ *
+ * @since  1.0.0
+ */
+class FlexformsViewForm extends F0FView
+{
+    public function display($tpl = null)
+    {
+        return;
+    }
+}
