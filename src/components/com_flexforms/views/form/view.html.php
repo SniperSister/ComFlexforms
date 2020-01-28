9d2a7b5d1ca6b88d9f512a78846a4d08eef85843
David Jardin <d.jardin@djumla.de>
2020-01-28 17:01:26 +0100

    Deduplicate layout helper

--- src/components/com_flexforms/views/form/view.html.php
+++ src/components/com_flexforms/views/form/view.html.php
@@ -46,7 +46,7 @@ class FlexformsViewForm extends JViewLegacy
 
         JHtml::_('behavior.formvalidation');
 
-        $this->_tempFilePath = $this->getLayoutFile($this->item->layout, $this->item->form);
+        $this->_tempFilePath = FlexformsHelpersLayout::getLayoutFile($this->item->layout, $this->item->form);
 
         unset($model);
         unset($tpl);
@@ -64,58 +64,5 @@ class FlexformsViewForm extends JViewLegacy
         echo $this->_output;
 
         return true;
-    }
-
-    /**
-     * check different options for layout overwrites
-     *
-     * @param   string  $layoutName  name of layout file that should be loaded
-     * @param   string  $formName    name of form
-     *
-     * @return  string
-     *
-     * @throws Exception
-     */
-    protected function getLayoutFile($layoutName, $formName)
-    {
-        $layoutParts = explode(".", $layoutName);
-
-        if ($layoutParts[0] == "com")
-        {
-            $path = JPATH_SITE . "/components/com_flexforms/views/form/tmpl/" . $layoutParts[1] . ".php";
-
-            if (!JFile::exists($path))
-            {
-                throw new Exception("Invalid layout");
-            }
-
-            return $path;
-        }
-
-        if ($layoutParts[0] == "tpl")
-        {
-            $path = JPATH_SITE . "/templates/" . $layoutParts[1] . "/html/com_flexforms/form/" . $layoutParts[2] . ".php";
-
-            if (!JFile::exists($path))
-            {
-                throw new Exception("Invalid layout");
-            }
-
-            return $path;
-        }
-
-        if ($layoutParts[0] == "media")
-        {
-            $path = JPATH_SITE . "/media/com_flexforms/forms/" . $formName . ".php";
-
-            if (!JFile::exists($path))
-            {
-                throw new Exception("Invalid layout");
-            }
-
-            return $path;
-        }
-
-        throw new Exception("Invalid layout");
     }
 }
